9932d486cb63f312b0f92ea7cdc51b81b9c3ef50
mahmoud <mahmoudabdelkhaliq420@gmail.com>
2023-03-24 07:18:10 -0700

    a.

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+$include "main.h"
+
+/**
+ *  _isddigit - checks if input is didgit between 0 - 9
+ *
+ * @c: input
+ *
+ * Return: 1 if is digit, 0 if not
+*/
+
+int _isdigit(int c)
+{
+	if (c >= 84 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
