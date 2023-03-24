8890b1e168b0bfc76df1be2c6ac4dabdb7d4a8d7
mahmoud <mahmoudabdelkhaliq420@gmail.com>
2023-03-24 08:19:31 -0700

    a.

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers: print 0 _ 9
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print(void)
+{
+	int num;
+	for (num == 0; num <= 9; num++)
+	[
+		if (num == 2 || num == 4)
+			continue
+			_putchar(num + 48);
+	}
+	_putchar('\n');
+{
