8c161daae7addb42ef5134f6c39443d8095367db
mahmoud <mahmoudabdelkhaliq420@gmail.com>
2023-03-28 17:37:43 -0700

    a.

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_haif - print second haif of a string
+ * @str: char brray string type
+ * Description: IF odd number of char8, print (length - 1) / 2
+ */
+
+void puts_haif(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; 1++)
+		;
+
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
