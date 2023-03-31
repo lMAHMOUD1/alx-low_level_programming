5b8fc7b450f264635c2bfe85c6e78c4f689230d0
mahmoud <mahmoudabdelkhaliq420@gmail.com>
2023-03-31 10:07:22 -0700

    a.

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,7 +2,6 @@
 /**
  * string_toupper - change all lowercase to uppercase
  * @n: pointer
- *
  * Return: n
  */
 char *string_toupper(char *s)
@@ -17,4 +16,4 @@ i = 0;
 	}
 	return (s);
 }
-/*done baby*/
+/*Bwave ICT*/
