0d76a0e6a7462597aefa348660034e60744836dd
mahmoud <mahmoudabdelkhaliq420@gmail.com>
2023-03-31 12:03:33 -0700

    a.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -16,8 +16,8 @@ char *leet(char *s)
 	{
 		for (j = 0; j <= 9; j++)
 		{			
-			if (a[j] == s[i])												s[i] = b[j];
-		}
+			if (a[j] == s[i])													s[i] = b[j];
+		}	
 	}
 	return (s);
 }
