c50b77d90c1499b4721238018e761b1218776ccb
mahmoud <mahmoudabdelkhaliq420@gmail.com>
2023-03-31 11:41:53 -0700

    a.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,25 +1,25 @@
 #include "main.h"
 
 /**
- * leet - encodes a string into 1337
- * @s: string to encode
- *
- * Return: address of s
- */
+ *  * leet - encodes a string into 1337
+ *   * @s: string to encode
+ *    *
+ *     * Return: address of s
+ *      */
 char *leet(char *s)
 {
 	int i, j;
 	char a[] = "aAeEoOtTlL";
-  	char b[] = "4433007711";
+	char b[] = "4433007711";
 
-  	for (i = 0; *(s + i); i++)
-    	{
-      		for (j = 0; j <= 9; j++)
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j <= 9; j++)
 		{
-	  		if (a[j] == s[i])
-	    		s[i] = b[j];
-		}
-    	}
-  	return (s);
+																if (a[j] == s[i])
+																s[i] = b[j];
+															}
+	}
+	return (s);
 }
-/* Doing hard things is a must *
+/* Doing hard things is a must */
