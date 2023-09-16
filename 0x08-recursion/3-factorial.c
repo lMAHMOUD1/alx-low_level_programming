e82b5651893030a7aa667fae46226a0b16a8f394
mahmoud <you@example.com>
2023-09-16 10:43:04 -0700

    a.

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * factorial _ Calculate the factorial of number
+ * factorial _ Calculate the factorial of a number
  * @n: the number to calculate the factorial
  *
  * Return: integer value
