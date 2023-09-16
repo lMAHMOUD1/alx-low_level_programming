bad4e39ed655e8a6e1b9bc8c7e2f61c345f026b1
mahmoud <you@example.com>
2023-09-16 04:42:06 -0700

    void _puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion _ print a string follo wed by a new line
+ * @s: the stringe to print
+ *
+ * Return: Nothing.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
