d43eeb1e1e9e63136c1248552b311bb1295f3b29
mahmoud <you@example.com>
2023-09-16 10:27:01 -0700

    a.

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,14 +6,10 @@
  *
  *  *Return: Nothing.
  */
-void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s > '\0')
 	{
-		return;
-	}
-	s++;
-	_print_rev_recursion(s);
-	s--;
+	_print_rev_recursion(s + 1);
 	_putchar(*s);
 }
